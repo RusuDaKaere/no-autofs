0de96fc8718c7d439a13bbf266f172e1a7739e6e
rusu_da_kaere(OneInAGoogolplex) <li0arq.r0a@gmail.com>
2019-07-20 16:19:18 +0900

    after refactering

--- src/index.js
+++ src/index.js
@@ -5,21 +5,28 @@ import ReactDOM from "react-dom";
 import ColorfulMsg from "./components/ColorfulMsg";
 
 //コンポーネント 関数的な定義
-// Functional conponent
-// style={{color:"blue"}}
-//１つめの｛｝:JS用，2つ目の:｛｝オブジェクト用
-const App = () => {
-  return (
-    <>
-      {/*これがJSXでのコメントだ*/}
-      <h2>REACTハンズオン！</h2>
-      <ColorfulMsg color="blue" message="Reactは難しくないよ～" />
-      <ColorfulMsg color="green" message="Vueも難しくないよ～" />
-      <ColorfulMsg color="red" message="Angularは，，，" />
-      <ColorfulMsg color="red">testtest</ColorfulMsg>
-    </>
-  );
-};
+// class
+// stateを使いたければ，classにする(最新型のReactでは関数でも行ける)
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+
+  // class にはrenderが必要
+  render() {
+    return (
+      <>
+        {/*これがJSXでのコメントだ*/}
+        <h2>REACTハンズオン！</h2>
+        <ColorfulMsg color="blue" message="Reactは難しくないよ～" />
+        <ColorfulMsg color="green" message="Vueも難しくないよ～" />
+        <ColorfulMsg color="red" message="Angularは，，，" />
+        <ColorfulMsg color="red">testtest</ColorfulMsg>
+      </>
+    );
+  }
+}
 
 //第一引数：コンポーネント，第2引数index.htmlに書かれてるId
 ReactDOM.render(<App />, document.getElementById("root"));
